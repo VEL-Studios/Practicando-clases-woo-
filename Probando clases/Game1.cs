cedd95eff3e385af282f180c7d31526bcf68222d
Tertyerty <vane.97x3@hotmail.com>
2015-07-13 16:53:42 -0500

    comentarios sfddsfsaasd

--- Probando clases/Game1.cs
+++ Probando clases/Game1.cs
@@ -23,10 +23,13 @@ namespace Probando_clases
         /// related content.  Calling base.Initialize will enumerate through any components
         /// and initialize them as well.
         /// </summary>
+       
         protected override void Initialize () {
             // TODO: Add your initialization logic here
 
             base.Initialize();
+            //sadasd
+            //jaksdjksald
         }
 
         /// <summary>
